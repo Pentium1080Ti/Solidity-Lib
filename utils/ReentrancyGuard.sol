4aff275a439a8742af74450fc4cf44beb677b94f
Pentium1080Ti <29131780+Pentium1080Ti@users.noreply.github.com>
2021-08-10 15:59:16 +0100

    Create ReentrancyGuard.sol

---
+++ utils/ReentrancyGuard.sol
@@ -0,0 +1,29 @@
+pragma solidity ^0.5.17;
+
+contract ReentrancyGuard {
+    // uint256 used over boolean as it takes an extra SLOAD (more gas) to read
+    uint256 private constant _NOT_ENTERED = 1;
+    uint256 private constant _ENTERED = 2;
+
+    uint256 private _status;
+
+    constructor () internal {
+        _status = _NOT_ENTERED;
+    }
+
+    /**
+     * @dev Prevents a contract from calling itself, directly or indirectly.
+     * Calling a `nonReentrant` function from another `nonReentrant`
+     * function is not supported. It is possible to prevent this from happening
+     * by making the `nonReentrant` function external, and make it call a
+     * `private` function that does the actual work.
+     */
+    modifier nonReentrant() {
+        require(_status != _ENTERED, "ReentrancyGuard: reentrant call");
+        _status = _ENTERED;
+        _;
+        // By storing the original value once again, a refund is triggered
+        _status = _NOT_ENTERED;
+    }
+    
+}
